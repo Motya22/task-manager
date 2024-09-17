b6f1cbda12da5f6fd9f6c7147dbefccd48e847d6
Matvei <motya2222@gmail.com>
2024-09-17 15:33:54 +0300

    refactor: move Login component logic to custom hook useLogin

--- src/features/auth/ui/login/Login.tsx
+++ src/features/auth/ui/login/Login.tsx
@@ -1,57 +1,12 @@
 import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from "@mui/material"
-import { useAppDispatch } from "common/hooks"
-import { BaseResponse } from "common/types"
-import { FormikHelpers, useFormik } from "formik"
 import React from "react"
-import { useSelector } from "react-redux"
 import { Navigate } from "react-router-dom"
-import { LoginParamsType } from "../../api/authApi"
-import { authThunks, selectIsLoggedIn } from "../../model/authSlice"
 import s from "./Login.module.css"
+import { useLogin } from "features/auth/lib/useLogin"
 
-type FormikErrorType = {
-  email?: string
-  password?: string
-  rememberMe?: boolean
-}
 
 export const Login = () => {
-  const dispatch = useAppDispatch()
-
-  const isLoggedIn = useSelector(selectIsLoggedIn)
-
-  const formik = useFormik({
-    validate: (values) => {
-      const errors: FormikErrorType = {}
-      if (!values.email) {
-        errors.email = "Email is required"
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = "Invalid email address"
-      }
-
-      if (!values.password) {
-        errors.password = "Required"
-      } else if (values.password.length < 3) {
-        errors.password = "Must be 3 characters or more"
-      }
-
-      return errors
-    },
-    initialValues: {
-      email: "",
-      password: "",
-      rememberMe: false,
-    },
-    onSubmit: (values, formikHelpers: FormikHelpers<LoginParamsType>) => {
-      dispatch(authThunks.login(values))
-        .unwrap()
-        .catch((reason: BaseResponse) => {
-          reason.fieldsErrors?.forEach((fieldError) => {
-            formikHelpers.setFieldError(fieldError.field, fieldError.error)
-          })
-        })
-    },
-  })
+  const {formik, isLoggedIn} = useLogin()
 
   if (isLoggedIn) {
     return <Navigate to={"/"} />
